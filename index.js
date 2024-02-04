c95c0c1b0858649960e82389a0c638318de8fc31
Dominic nix <dnix8715@gmail.com>
2024-02-04 13:45:34 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;  
+var random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+var num3 = 20;
+var num4 = 8;
+var mod = num3 % num4;
+var num1 = 10;
+var num2 = 15;
+var num3 = 20;
+var num4 = 5;
+var max = Math.max(num1, num2, num3, num4);
